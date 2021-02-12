c947a81d6da9a4de65083e2dbc1ce2933381cec5
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-12 16:41:57 +0530

    Create project_main.c
    Adding Project_Main.c Skeleton file

---
+++ 3_Implementation/project_main.c
@@ -0,0 +1,57 @@
+//include files here header
+#define VALID   (1)
+#define INVALID (0)
+enum operations{ Basic_Calculations=1,Trignometry,Dimension_Conversion,Binary_Decimal_Conversion, EXIT };
+unsigned int calculator_operation ;
+void calculator_menu(void);
+
+int valid_operation(int operation);
+
+int main(int argc, char *argv[])
+{
+    printf("\n****Calculator****\n");
+    while(1)
+    {
+        calculator_menu();
+    }
+}
+void calculator_menu(void)
+{
+    printf("\nAvailable Operations\n");
+    printf("\n 1.Basic Calculations\n 2.Trigonometry\n3.Dimension Conversion\n4.Binary Decimal Conversion\n5. Exit");
+    printf("\n\tEnter your choice\n");
+   
+
+    scanf("%d", &calculator_operation);
+
+    switch(calculator_operation)
+    {
+        case 1:
+             
+
+            basic();
+            
+
+            break;
+        case 2:
+
+            trig();
+            
+
+            break;
+        case 3:
+        dim_conv();
+
+            break;
+        case 4:
+        bin_conv();
+
+            break;
+        case 5: 
+            exit(0);
+            break;
+        default:
+            printf("\nPlease choose correct options\n");
+
+    }
+}
