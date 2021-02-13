ac66b787929a37db53568c0f0283bdb78e908bc2
99003754 <78854545+99003754@users.noreply.github.com>
2021-02-13 14:02:25 +0530

    Update bin_conv.h

--- 3_Implementation/inc/bin_conv.h
+++ 3_Implementation/inc/bin_conv.h
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 #include<stdlib.h>
-long dectobin();
+void dectobin();
 long dectohexa(long quotient);
 long decimaltobin(long binary);
 long binarytodec(long  bintodec_val);
