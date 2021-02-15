4f5978e51e850d10037be32ecba919ebc08a5cd7
99003755 <rohan.roy@ltts.com>
2021-02-15 09:12:35 +0530

    Modifications in Dim.c

--- 3_Implementation/src/dim.c
+++ 3_Implementation/src/dim.c
@@ -1,6 +1,41 @@
 
 #include"dim.h"
 float cm_to_m(float );
+float cm_to_km(float );
+float cm_to_inch(float );
+float cm_to_foot(float );
+
+float m_to_cm(float );
+float m_to_km(float );
+float m_to_inch(float );
+float m_to_foot(float );
+
+float km_to_cm(float );
+float km_to_m(float );
+float km_to_inch(float );
+float km_to_foot(float );
+
+float inch_to_cm(float );
+float inch_to_m(float );
+float inch_to_km(float );
+float inch_to_foot(float );
+
+float foot_to_cm(float );
+float foot_to_m(float );
+float foot_to_km(float );
+float foot_to_inch(float );
+
+float g_to_kg(float );
+float g_to_pd(float );
+
+float kg_to_g(float );
+float kg_to_pd(float );
+
+float pd_to_g(float );
+float pd_to_kg(float );
+
+float F_to_C(float );
+float C_to_F(float );
 
 void dim_con()
 {
@@ -408,7 +443,7 @@ float m_to_cm(float conv_inp)
 
 float m_to_km(float conv_inp)
 {
-    return (conv_inp*1000);
+    return (conv_inp/1000);
    
 }
 
